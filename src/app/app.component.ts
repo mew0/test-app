3c8a8fa992fa61b079666f362a90db484cb080b5
mew0 <tearless.bc@mail.ru>
2025-02-06 17:27:23 +0500

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ProfileCardComponent} from './common-ui/profile-card/profile-card.component';
-import {ProfileService} from './data/services/profile.service';
-import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test';
 
-  profileService = inject(ProfileService)
-  profiles: Profile[] = []
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe(val => {
-      this.profiles = val;
-    })
-  }
 }
